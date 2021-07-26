11e6ec04b2faef31a95d5fc4ba92307bd31803c8
JooYoung <jykim0747@gmail.com>
2021-07-26 20:51:56 +0900

    [#4] add Permission Check sample

---
+++ app/src/main/java/com/example/diyapp/PermissionCheck.kt
@@ -0,0 +1,26 @@
+package com.example.diyapp
+
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionCheck(val permissionActivity: Activity, val requirePermissions: Array<String>){
+
+    private val permissionRequestCode = 100
+
+    public fun permissionCheck(){
+        val failRequestPermissionList = ArrayList<String>()
+
+        for(permission in requirePermissions){
+            if(ContextCompat.checkSelfPermission(permissionActivity.applicationContext, permission) != PackageManager.PERMISSION_GRANTED){
+                failRequestPermissionList.add(permission)
+            }
+        }
+
+        if(failRequestPermissionList.isNotEmpty()) {
+            val array = arrayOfNulls<String>(failRequestPermissionList.size)
+            ActivityCompat.requestPermissions(permissionActivity, failRequestPermissionList.toArray(array), permissionRequestCode)
+        }
+    }
+}
