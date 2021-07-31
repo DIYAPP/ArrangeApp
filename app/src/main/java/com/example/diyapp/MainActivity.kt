d8302fe2293a2fb208d7ba6661a38943d6c21668
JooYoung <jykim0747@gmail.com>
2021-07-31 15:51:38 +0900

    [#2] add Sub layout  * 버튼 클릭 시 변경되는 레이아웃 추가  * 버튼 이름 변경  * 레이아웃 이름 변경 예정

--- app/src/main/java/com/example/diyapp/MainActivity.kt
+++ app/src/main/java/com/example/diyapp/MainActivity.kt
@@ -1,21 +1,42 @@
 package com.example.diyapp
 
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val al = AppList(this.applicationContext)
-        val itemList : ArrayList<item> = al.getAppList()
-        val itemAdapter = MainListAdapter(this, itemList)
-        main_item.adapter = itemAdapter
-
+    /** 버튼 클릭시 레이아웃 변경 */
+    override fun onClick(v: View?) {
+        when(v?.id)
+        {
+            R.id.buttonCurrent->{
+                var tView = findViewById<TextView>(R.id.buttonCurrent)
+                tView.setText("짠")
+            }
+            R.id.buttonABC->{
+                val intent = Intent(this, SubActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.buttonColor->{
+                val intent = Intent(this, SubActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.buttonCategory->{
+                val intent = Intent(this, SubActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
