ba2538cd2100d7cea816528731f83cc0ec2a33f1
DONGJIN KIM <ehdwls43@gmail.com>
2021-07-31 20:19:21 +0900

    #8 button click and view app list now app, abc_arrange

--- app/src/main/java/com/example/diyapp/MainActivity.kt
+++ app/src/main/java/com/example/diyapp/MainActivity.kt
@@ -1,39 +1,55 @@
 package com.example.diyapp
 
-
+import android.app.Application
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.diyapp.sortmethod.Companion.method
 import kotlinx.android.synthetic.main.activity_main.*
 
+/** 정렬 방식을 변수로 구분 **/
+class sortmethod: Application() {
+    lateinit var context: Context
+    init{method=0}
+    companion object{
+        var method = 0
+    }
+}
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
-    /** 버튼 클릭시 레이아웃 변경 */
+    /** 버튼 클릭시 레이아웃 변경 + 버튼 클릭시 정렬 방식 변경*/
     override fun onClick(v: View?) {
         when(v?.id)
         {
             R.id.buttonCurrent->{
-                var tView = findViewById<TextView>(R.id.buttonCurrent)
-                tView.setText("짠")
+                method = 0
+                val intent = Intent(this, SubActivity::class.java)
+                startActivity(intent)
+                //var tView = findViewById<TextView>(R.id.buttonCurrent)
+                //tView.setText("Your app")
             }
             R.id.buttonABC->{
+                method = 1
                 val intent = Intent(this, SubActivity::class.java)
                 startActivity(intent)
             }
             R.id.buttonColor->{
+                method = 2
                 val intent = Intent(this, SubActivity::class.java)
                 startActivity(intent)
             }
             R.id.buttonCategory->{
+                method = 3
                 val intent = Intent(this, SubActivity::class.java)
                 startActivity(intent)
             }
--- app/src/main/java/com/example/diyapp/SubActivity.kt
+++ app/src/main/java/com/example/diyapp/SubActivity.kt
@@ -1,11 +1,36 @@
 package com.example.diyapp
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.diyapp.sortmethod.Companion.method
+import kotlinx.android.synthetic.main.activity_arrange.*
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arrange)
+
+        var itemlist = arrayListOf<item>()
+
+        val packageManager = this.packageManager
+        val packages: List<PackageInfo> = packageManager.getInstalledPackages(0)
+
+        for (info: PackageInfo in packages)
+        {
+        val iticon: Drawable = info.applicationInfo.loadIcon(packageManager)
+        val it: item = item(info.applicationInfo.processName, iticon)
+        itemlist.add(it)
+        }
+        if(method==1)
+        {
+           itemlist= itemlist.reversed() as ArrayList<item>
+        }
+        else{}
+        val itemAdapter = MainListAdapter(this, itemlist)
+        mainListView.adapter = itemAdapter
     }
 }
