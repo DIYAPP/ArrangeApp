765850aed9997a6822e479dc12551c6c8d51abf3
DONGJIN KIM <ehdwls43@gmail.com>
2021-08-01 03:31:44 +0900

    #8 change sorting abc_arrange

--- app/src/main/java/com/example/diyapp/SubActivity.kt
+++ app/src/main/java/com/example/diyapp/SubActivity.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.content.pm.PackageInfo
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.telephony.ims.ImsMmTelManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diyapp.sortmethod.Companion.method
 import kotlinx.android.synthetic.main.activity_arrange.*
+import kotlinx.android.synthetic.main.main_item.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +31,8 @@ class SubActivity : AppCompatActivity() {
         }
         if(method==1)
         {
-           itemlist= itemlist.reversed() as ArrayList<item>
+            itemlist =  ArrayList(itemlist.sortedBy { it.name })
+           //itemlist = itemlist.reversed() as ArrayList<item>
         }
         else{}
         val itemAdapter = MainListAdapter(this, itemlist)
