e016eba0aa8024f677c069f216660a215610c249
DONGJIN KIM <ehdwls43@gmail.com>
2021-08-15 20:06:54 +0900

    Palette color extract funtion

--- app/src/main/java/com/example/diyapp/SubActivity.kt
+++ app/src/main/java/com/example/diyapp/SubActivity.kt
@@ -1,17 +1,19 @@
 package com.example.diyapp
 
-import android.content.Context
+
 import android.content.pm.PackageInfo
-import android.graphics.drawable.Drawable
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.*
 import android.os.Bundle
-import android.telephony.ims.ImsMmTelManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.palette.graphics.Palette
 import com.example.diyapp.sortmethod.Companion.method
 import kotlinx.android.synthetic.main.activity_arrange.*
-import kotlinx.android.synthetic.main.main_item.*
-import java.util.*
+
 import kotlin.collections.ArrayList
+
+class item ( val name : String, val image : Drawable, val color : Int )
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +27,14 @@ class SubActivity : AppCompatActivity() {
 
         for (info: PackageInfo in packages)
         {
+        var color_number = 0
         val iticon: Drawable = info.applicationInfo.loadIcon(packageManager)
-        val it: item = item(info.applicationInfo.processName, iticon)
+            Palette.from(bitmap_icon(iticon)).generate{ palette ->
+                if(palette != null) {
+                    color_number = palette.getDominantColor(R.attr.color)
+                }
+            }
+        val it: item = item(info.applicationInfo.processName, iticon,color_number)
         itemlist.add(it)
         }
         if(method==1)
@@ -34,8 +42,53 @@ class SubActivity : AppCompatActivity() {
             itemlist =  ArrayList(itemlist.sortedBy { it.name })
            //itemlist = itemlist.reversed() as ArrayList<item>
         }
-        else{}
+        else if(method==2){
+            itemlist =  ArrayList(itemlist.sortedBy { it.color })
+        }
         val itemAdapter = MainListAdapter(this, itemlist)
         mainListView.adapter = itemAdapter
     }
+}
+fun bitmap_icon(iticon : Drawable): Bitmap
+{
+    var color : Bitmap
+
+    if (iticon is AdaptiveIconDrawable) {
+        val backgroundDrawable = (iticon as AdaptiveIconDrawable).background  // 1
+        val foregroundDrawable = (iticon as AdaptiveIconDrawable).foreground  // 2
+
+        val drawables = arrayOfNulls<Drawable>(2)
+        drawables[0] = backgroundDrawable
+        drawables[1] = foregroundDrawable
+
+        val layerDrawable = LayerDrawable(drawables)  // 3
+        val width = layerDrawable.intrinsicWidth
+        val height = layerDrawable.intrinsicHeight
+        val bitmap = Bitmap.createBitmap(
+            width, height, Bitmap.Config.ARGB_8888)  // 4
+        val canvas = Canvas(bitmap)  // 5
+        layerDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+        layerDrawable.draw(canvas)  // 6
+        color = bitmap
+    }
+    else if(iticon is VectorDrawable)
+    {
+        val bitmap = Bitmap.createBitmap(
+            iticon.getIntrinsicWidth(),
+            iticon.getIntrinsicHeight(), Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        iticon.setBounds(0, 0, canvas.width, canvas.height)
+        iticon.draw(canvas)
+
+        color = bitmap
+    }
+    else
+    {
+        val bitmapDrawable = iticon as BitmapDrawable
+        val bitmap = bitmapDrawable.bitmap
+        color = bitmap
+    }
+
+    return color
 }
